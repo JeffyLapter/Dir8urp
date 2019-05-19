895b82997abcfc5e6bb2afb760adae6416de3a9c
HC1024 <45419557+HC1024@users.noreply.github.com>
2019-05-19 21:33:06 +0800

    Create Get_302_info.py
    获取302跳转信息的函数，之后整合到moudle中

---
+++ Get_302_info.py
@@ -0,0 +1,15 @@
+def get_302_info(domain):
+    try:
+        page_302 = requests.get(domain,headers=header,allow_redirects = False,timeout=3)
+        if page_302.status_code == 302:
+            redirect_domain = page_302.headers['Location']
+            print("[!] 302 redirect from {} to {}".format(domain, redirect_domain))
+            return redirect_domain
+        else:
+            return False
+    except requests.exceptions.ConnectTimeout:
+        print("Connect time out")
+        return False
+    except requests.exceptions.ConnectionError:
+        print("Unknown server")
+        return False
